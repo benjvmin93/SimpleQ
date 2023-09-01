c5a9d56305d2b66e246f231f33b7a8313b0a09e1
Max <bomax1597@gmail.com>
2023-09-01 20:15:10 +0200

    defined the circuit types

--- src/API/Types.py
+++ src/API/Types.py
@@ -3,3 +3,17 @@ from pydantic import BaseModel
 
 class TestType(BaseModel):
     name: str
+
+
+class Gate(BaseModel):
+    type: str
+    ctrl: list[int]
+
+
+class Column(BaseModel):
+    index: int
+    gate: Gate
+
+
+class Circuit(BaseModel):
+    circuit: list[Column]
