6be1279e736ed360e1e807836459833e3c7572fc
Benjamin Guichard <benjamin.guichard@epita.fr>
2023-09-26 15:40:14 +0200

    cleaned code and update qubit class

--- src/QLibrary/SimpleQ/qubit.py
+++ src/QLibrary/SimpleQ/qubit.py
@@ -5,27 +5,13 @@ class Qubit:
         """
         By default, a qubit is initialized in the state |0>
         """
-        self.ALPHA = 1 + 0 * 1j
-        self.BETA = 0 + 0 * 1j
+        self.state_vector = np.array([1, 0], dtype=complex)
 
     def get_state_vector(self):
-        return np.array([[self.ALPHA, self.BETA]])
-
-    def get_alpha(self):
-        return self.ALPHA
-
-    def get_beta(self):
-        return self.BETA
-    
-    def set_alpha(self, alpha):
-        self.ALPHA = alpha
-        
-    def set_beta(self, beta):
-        self.BETA = beta
+        return self.state_vector()
 
     def set_state_vector(self, state_vector):
-        self.ALPHA = state_vector[0][0]
-        self.BETA = state_vector[0][1]
+        self.state = state_vector
 
     def get_str_state(self):
-        return f"{self.ALPHA}|0> + {self.BETA}|1>"
+        return f"{self.state_vector[0]}|0> + {self.state_vector[1]}|1>"
